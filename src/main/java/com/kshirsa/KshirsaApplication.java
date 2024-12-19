936764971ba232bd06e127efb67c3fd7776be6f9
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-12-19 14:06:47 +0530

    Initial Commit

---
+++ src/main/java/com/kshirsa/KshirsaApplication.java
@@ -0,0 +1,13 @@
+package com.kshirsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KshirsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KshirsaApplication.class, args);
+	}
+
+}
