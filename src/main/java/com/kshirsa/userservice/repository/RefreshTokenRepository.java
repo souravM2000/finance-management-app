3892858ff3f886599f9be62e901303dff492771c
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-12-24 19:06:53 +0530

    Implemented complete signup & login flow in user service

--- src/main/java/com/kshirsa/userservice/repository/RefreshTokenRepository.java
+++ src/main/java/com/kshirsa/userservice/repository/RefreshTokenRepository.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 	Optional<RefreshToken> findByToken(String token);
-	void deleteByEmail(String email);
 }
