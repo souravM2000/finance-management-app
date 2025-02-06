51ed5652b63765933e025b4cba8c8deb22658750
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2025-02-06 23:41:52 +0530

    Added success response messages and refactored controller methods for tracking operations

--- src/main/java/com/kshirsa/trackingservice/controller/TrackingAddController.java
+++ src/main/java/com/kshirsa/trackingservice/controller/TrackingAddController.java
@@ -1,11 +1,14 @@
 package com.kshirsa.trackingservice.controller;
 
 import com.kshirsa.coreservice.BaseConstants;
+import com.kshirsa.coreservice.BaseController;
+import com.kshirsa.coreservice.SuccessResponse;
 import com.kshirsa.coreservice.exception.CustomException;
 import com.kshirsa.trackingservice.dto.request.AddCategory;
 import com.kshirsa.trackingservice.dto.request.AddLoanRepayment;
 import com.kshirsa.trackingservice.dto.request.AddTransaction;
 import com.kshirsa.trackingservice.entity.Category;
+import com.kshirsa.trackingservice.entity.LoanRepayment;
 import com.kshirsa.trackingservice.entity.Transactions;
 import com.kshirsa.trackingservice.service.declaration.TrackingAddService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,6 +16,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,32 +27,33 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @SecurityRequirement(name = "Bearer Authentication")
 @Tag(name = "3. Tracking Add Controller", description = "APIs for adding money tracking data")
-public class TrackingAddController {
+public class TrackingAddController extends BaseController {
 
     private final TrackingAddService trackingAddService;
+    private final Environment env;
 
     @Operation(summary = "Add a new category")
     @PostMapping("/category")
-    public Category addCategory(@RequestBody AddCategory category) {
-        return trackingAddService.addCategory(category);
+    public ResponseEntity<SuccessResponse<Category>> addCategory(@RequestBody AddCategory category) {
+        return ok(trackingAddService.addCategory(category), env.getProperty("CATEGORY.ADD.SUCCESS"));
     }
 
     @Operation(summary = "Add a new hash tag")
     @PostMapping("/hash-tag")
-    public String addHashTag(@RequestParam String transactionId,
-                             @RequestParam String hashTag) throws CustomException {
-        return trackingAddService.addHashTag(transactionId, hashTag);
+    public ResponseEntity<SuccessResponse<String>> addHashTag(@RequestParam String transactionId,
+                                                              @RequestParam String hashTag) throws CustomException {
+        return ok(trackingAddService.addHashTag(transactionId, hashTag), env.getProperty("HASH.TAG.ADD.SUCCESS"));
     }
 
     @Operation(summary = "Add a new transaction")
     @PostMapping("/transaction")
-    public Transactions addTransaction(@RequestBody @Valid AddTransaction transaction) throws CustomException {
-        return trackingAddService.addTransaction(transaction);
+    public ResponseEntity<SuccessResponse<Transactions>> addTransaction(@RequestBody @Valid AddTransaction transaction) throws CustomException {
+        return ok(trackingAddService.addTransaction(transaction), env.getProperty("TRANSACTION.ADD.SUCCESS"));
     }
 
     @Operation(summary = "Add a new loan repayment")
     @PostMapping("/loan-repayment")
-    public void addLoanRepayment(@RequestBody @Valid AddLoanRepayment loanRepayment) throws CustomException {
-        trackingAddService.addLoanRepayment(loanRepayment);
+    public ResponseEntity<SuccessResponse<LoanRepayment>> addLoanRepayment(@RequestBody @Valid AddLoanRepayment loanRepayment) throws CustomException {
+        return ok(trackingAddService.addLoanRepayment(loanRepayment), env.getProperty("LOAN.REPAYMENT.ADD.SUCCESS"));
     }
 }
